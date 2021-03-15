059ebc0bb37afe5f8a0468818154ad463e2a820a
Mario <36368155+marioallaa@users.noreply.github.com>
2021-03-15 15:32:21 +0000

    Create index.ml

---
+++ index.ml
@@ -0,0 +1,50 @@
+## Inovacion Sh.P.K
+
+# `Movie recommendation App`
+
+## Description
+Forge this repository and create a frontend web application in Vue.js, React.js or plaint html that: 
+
+1. Ask the user for a query.
+2. Searchs for a movie related to that query.
+    - Interactive search bar.
+3. Use theMovieDB API to search for the movies.
+    - Show the movies in a card view 
+        - Title
+        - Image
+        - Description
+        - Ratings
+        - Relase date
+        - Button: Suggest similar movies
+4. Handle button click
+5. Use theMovieDb API to get recommendations for similar movies. 
+    - Show the movies in a card view
+        - Title
+        - Image
+        - Description
+        - Ratings
+        - Relase date
+    - Offer the possibility to sort according to the ratings of the suggested movies
+
+
+<hr>
+
+## Resources
+
+### Vue.js 
+`official page: ` https://vuejs.org/
+
+
+### React.js 
+`official page: ` https://reactjs.org/
+
+
+### The Movie DB API Documentation
+
+`search movies: `
+https://developers.themoviedb.org/3/search/search-movies 
+
+`get recommendations: `
+https://developers.themoviedb.org/3/movies/get-movie-recommendations
+
+
